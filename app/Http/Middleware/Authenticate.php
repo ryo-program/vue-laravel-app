5fd5c6c58a14bc5503940734fbb1ec638ef68c1d
ryo-program <yh.1312.sun@gmail.com>
2020-07-11 20:48:02 +0900

    ログイン周り

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,10 +6,12 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    protected $admin_route = 'admin.login';
+
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route($this->admin_route);
         }
     }
 }
